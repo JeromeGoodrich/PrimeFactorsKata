9c8fdbe48a6084b9f003763b214dd480f0b082d2
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-01 16:13:58 -0700

    first commit

---
+++ PrimeFactorsKataTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace PrimeFactorsKataTest
+{
+    
+    public class PrimeFactorsTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
